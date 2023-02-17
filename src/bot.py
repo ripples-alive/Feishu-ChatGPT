11d3d8cd30708fd0f4024a8d6d0ca9e50dd1042a
ripples <564311873@qq.com>
2023-02-17 18:20:45 +0800

    feat: add more functions for bot

--- src/bot.py
+++ src/bot.py
@@ -16,11 +16,13 @@ from larksuiteoapi import Context
 from larksuiteoapi.event import handle_event
 from larksuiteoapi.model import OapiHeader
 from larksuiteoapi.model import OapiRequest
+from larksuiteoapi.service.contact.v3 import Service as ContactService
 from larksuiteoapi.service.im.v1 import MessageReceiveEvent
 from larksuiteoapi.service.im.v1 import MessageReceiveEventHandler
 from larksuiteoapi.service.im.v1 import Service as ImService
 from larksuiteoapi.service.im.v1 import model
 from revChatGPT.V1 import Chatbot
+from revChatGPT.V1 import Error as ChatbotError
 
 from file import read_json
 from file import write_json
@@ -37,7 +39,8 @@ app_settings = Config.new_internal_app_settings_from_env()
 conf = Config(DOMAIN_FEISHU, app_settings, log_level=LEVEL_DEBUG)
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
 
-service = ImService(conf)
+im_service = ImService(conf)
+contact_service = ContactService(conf)
 
 log = logging.getLogger("bot")
 
@@ -46,33 +49,69 @@ chatbot = Chatbot(read_json("chatbot.json"))
 queue = Queue()
 
 
+def get_conf(uuid):
+    db = read_json(DB_FILE, {})
+    return db.get(uuid, {})
+
+
+def set_conf(uuid, conf):
+    db = read_json(DB_FILE, {})
+    db[uuid] = conf
+    write_json(DB_FILE, db)
+
+
 def worker():
     while True:
-        text, conversation_id, message_id = queue.get()
-        msg = ask(text, conversation_id)
+        message_id, open_id, uuid, text = queue.get()
+        msg = ask(uuid, open_id, text)
         reply_message(message_id, msg)
 
 
-def ask(text, conversation_id):
-    db = read_json(DB_FILE, {})
-    conf = db.get(conversation_id, {})
+def ask(uuid, open_id, text):
+    conf = get_conf(uuid)
+    conversation_id = conf.get("conversation_id")
+    parent_id = conf.get("parent_id")
+    new_chat = conversation_id is None
+
+    # prevent bug in revChatGPT
+    chatbot.conversation_id = None
+    chatbot.parent_id = None
 
     msg = ""
     try:
-        for data in chatbot.ask(text, **conf):
+        for data in chatbot.ask(text, conversation_id=conversation_id, parent_id=parent_id):
             msg = data["message"]
+
+        conversation_id = data["conversation_id"]
+        parent_id = data["parent_id"]
+        conf = dict(conversation_id=conversation_id)
+        set_conf(uuid, conf)
+    except ChatbotError as e:
+        return f"{e.source}({e.code}): {e.message}"
+    except UnboundLocalError:
+        return "获取对话结果失败，请重试"
     except Exception:
         traceback.print_exc()
-        msg = "服务器异常，请重试"
+        return "服务器异常，请重试"
 
-    try:
-        db[conversation_id] = dict(conversation_id=data["conversation_id"], parent_id=data["parent_id"])
-    except KeyError:
-        db[conversation_id] = {}
-        msg = "对话出现异常，请重新开始"
-    write_json(DB_FILE, db)
+    if new_chat:
+        name = get_user_name(open_id)
+        title = f"{name} - {uuid}"
+        chatbot.change_title(conversation_id, title)
 
     return msg
+
+
+def get_user_name(open_id):
+    req_call = contact_service.users.get()
+    req_call.set_user_id(open_id)
+    resp = req_call.do()
+    log.debug(f"request id = {resp.get_request_id()}")
+    log.debug(f"http status code = {resp.get_http_status_code()}")
+    log.debug(f"header = {resp.get_header().items()}")
+    if resp.code != 0:
+        return "Unknown"
+    return resp.data.user.en_name
 
 
 def reply_message(message_id, msg):
@@ -80,7 +119,7 @@ def reply_message(message_id, msg):
     body.content = json.dumps(dict(text=msg))
     body.msg_type = "text"
 
-    req_call = service.messages.reply(body)
+    req_call = im_service.messages.reply(body)
     req_call.set_message_id(message_id)
 
     resp = req_call.do()
@@ -107,25 +146,44 @@ def message_receive_handle(ctx: Context, conf: Config, event: MessageReceiveEven
     text: str = json.loads(message.content).get("text")
     text = text.replace("@_user_1", "").strip()
 
-    user = event.event.sender.sender_id.user_id
+    open_id = event.event.sender.sender_id.open_id
+    uuid = f"{open_id}@{message.chat_id}"
     if text.startswith("/"):
         cmds = text.split()
         cmd = cmds[0]
         args = cmds[1:]
         if cmd == "/help":
             msg = "/help: 查看命令说明\n"
-            msg += "/reset: 清空对话\n"
+            msg += "/reset: 重新开始对话\n"
+            msg += "/delete: 删除当前对话\n"
+            msg += "/title: 修改对话标题\n"
         elif cmd == "/reset":
-            db = read_json(DB_FILE, {})
-            del db[user]
-            write_json(DB_FILE, db)
-            msg = "对话已清空"
+            set_conf(uuid, {})
+            msg = "对话已重新开始"
         else:
-            msg = "无效命令"
+            conf = get_conf(uuid)
+            conversation_id = conf.get("conversation_id")
+            if conversation_id is None:
+                msg = "对话不存在"
+            elif cmd == "/delete":
+                chatbot.delete_conversation(conversation_id)
+                set_conf(uuid, {})
+                msg = "成功删除对话"
+            elif cmd == "/title":
+                if args:
+                    title = args[0].strip()
+                    name = get_user_name(open_id)
+                    title = f"{name} - {title}"
+                    chatbot.change_title(conversation_id, title)
+                    msg = f"成功修改标题为：{title}"
+                else:
+                    msg = "标题不存在"
+            else:
+                msg = "无效命令"
 
         reply_message(message.message_id, msg)
     else:
-        queue.put_nowait((text, user, message.message_id))
+        queue.put_nowait((message.message_id, open_id, uuid, text))
 
 
 MessageReceiveEventHandler.set_callback(conf, message_receive_handle)
